4e40f3798fae0963db141587da97ce7af5a5282c
rexmas <rex.fenley@gmail.com>
2015-07-10 23:18:46 -0700

    [master] abstracted out our requests

--- Crust/API/CRUberProductsRequest.swift
+++ Crust/API/CRUberProductsRequest.swift
@@ -1,10 +1,22 @@
 import Alamofire
 
-struct CRUberProductsRequest {
+enum CRHTTPMethod : String {
+    case GET = "GET"
+}
+
+protocol CRUberRequestProtocol {
+    
+    var requestUrl : String { get }
+    var HTTPMethod : CRHTTPMethod { get }
+    
+    func send()
+}
+
+class CRUberRequest : CRUberRequestProtocol {
     func send() {
-        let URL = NSURL(string: "https://api.uber.com/v1/products?latitude=37.775&longitude=-122")!
+        let URL = NSURL(string: self.requestUrl)!
         let mutableURLRequest = NSMutableURLRequest(URL: URL)
-        mutableURLRequest.HTTPMethod = "GET"
+        mutableURLRequest.HTTPMethod = self.HTTPMethod.rawValue
         
         mutableURLRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
         mutableURLRequest.setValue("Token VN13RQ21ivJxCHatCirEK1461EEvwNqpfjccIN9-", forHTTPHeaderField: "Authorization")
@@ -15,65 +27,77 @@ struct CRUberProductsRequest {
                 println(response)
                 println(error)
         }
-
+    }
+    
+    var requestUrl : String {
+        get {
+            assertionFailure("Must override in subclass")
+            return ""
+        }
+    }
+    
+    var HTTPMethod : CRHTTPMethod {
+        get {
+            assertionFailure("Must override in subclass")
+            return CRHTTPMethod.GET
+        }
     }
 }
 
-struct CRUberPriceEstimatesRequest {
-    func send() {
-        let URL = NSURL(string: "https://api.uber.com/v1/estimates/price?start_latitude=37.775&start_longitude=-122&end_latitude=38&end_longitude=-122")!
-        let mutableURLRequest = NSMutableURLRequest(URL: URL)
-        mutableURLRequest.HTTPMethod = "GET"
-        
-        mutableURLRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        mutableURLRequest.setValue("Token VN13RQ21ivJxCHatCirEK1461EEvwNqpfjccIN9-", forHTTPHeaderField: "Authorization")
-        
-        Alamofire.request(mutableURLRequest)
-            .response { (request, response, data, error) in
-                println(request)
-                println(response)
-                println(error)
+class CRUberProductsRequest : CRUberRequest {
+    override var requestUrl : String {
+        get {
+            return "https://api.uber.com/v1/products?latitude=37.775&longitude=-122"
         }
-        
+    }
+    
+    override var HTTPMethod : CRHTTPMethod {
+        get {
+            return CRHTTPMethod.GET
+        }
+    }
+}
+
+class CRUberPriceEstimatesRequest : CRUberRequest {
+    override var requestUrl : String {
+        get {
+            return "https://api.uber.com/v1/estimates/price?start_latitude=37.775&start_longitude=-122&end_latitude=38&end_longitude=-122"
+        }
+    }
+    
+    override var HTTPMethod : CRHTTPMethod {
+        get {
+            return CRHTTPMethod.GET
+        }
     }
 }
 
 
-struct CRUberTimeEstimatesRequest {
-    func send() {
-        let URL = NSURL(string: "https://api.uber.com/v1/estimates/time?start_latitude=37.775&start_longitude=-122")!
-        let mutableURLRequest = NSMutableURLRequest(URL: URL)
-        mutableURLRequest.HTTPMethod = "GET"
-        
-        mutableURLRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        mutableURLRequest.setValue("Token VN13RQ21ivJxCHatCirEK1461EEvwNqpfjccIN9-", forHTTPHeaderField: "Authorization")
-        
-        Alamofire.request(mutableURLRequest)
-            .response { (request, response, data, error) in
-                println(request)
-                println(response)
-                println(error)
+class CRUberTimeEstimatesRequest : CRUberRequest {
+    override var requestUrl : String {
+        get {
+            return "https://api.uber.com/v1/estimates/time?start_latitude=37.775&start_longitude=-122"
         }
-        
+    }
+    
+    override var HTTPMethod : CRHTTPMethod {
+        get {
+            return CRHTTPMethod.GET
+        }
     }
 }
 
-struct CRUberPromotionsRequest {
-    func send() {
-        let URL = NSURL(string: "https://api.uber.com/v1/promotions?start_latitude=37.775&start_longitude=-122&end_latitude=38&end_longitude=-122")!
-        let mutableURLRequest = NSMutableURLRequest(URL: URL)
-        mutableURLRequest.HTTPMethod = "GET"
-        
-        mutableURLRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        mutableURLRequest.setValue("Token VN13RQ21ivJxCHatCirEK1461EEvwNqpfjccIN9-", forHTTPHeaderField: "Authorization")
-        
-        Alamofire.request(mutableURLRequest)
-            .response { (request, response, data, error) in
-                println(request)
-                println(response)
-                println(error)
+class CRUberPromotionsRequest : CRUberRequest {
+    override var requestUrl : String {
+        get {
+            return "https://api.uber.com/v1/promotions?start_latitude=37.775&start_longitude=-122&end_latitude=38&end_longitude=-122"
         }
-        
+    }
+    
+    override var HTTPMethod : CRHTTPMethod {
+        get {
+            return CRHTTPMethod.GET
+        }
     }
 }
 
