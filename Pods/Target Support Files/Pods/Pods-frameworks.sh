5d0c251d9f590077fb7d760e9ae0b3e9e4d74cf7
rexmas <rex.fenley@gmail.com>
2015-10-05 21:53:29 -0700

    [remove-swifty-json] deleted swifty json, pod update, updated alamofire

--- Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -53,9 +53,7 @@ code_sign_if_enabled() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework 'Pods/Alamofire.framework'
-  install_framework 'Pods/SwiftyJSON.framework'
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework 'Pods/Alamofire.framework'
-  install_framework 'Pods/SwiftyJSON.framework'
 fi
