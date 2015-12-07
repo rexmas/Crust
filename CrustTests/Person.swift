b997ca36e6ef9cb931f5edbdd1f29b5b7704c730
rexmas <rex.fenley@gmail.com>
2015-12-06 18:55:04 -0800

    [any-mappable] brushed up person for testing

--- CrustTests/Person.swift
+++ CrustTests/Person.swift
@@ -4,6 +4,7 @@ import Crust
 struct Person : AnyMappable {
     
     var bankAccounts: NSArray = [ 1234, 5678 ]
+    var attitude: String = "awesome"
 }
 
 class PersonMapping : AnyMapping {
@@ -14,7 +15,31 @@ class PersonMapping : AnyMapping {
         return []
     }
     
-    func mapping(tomap: Person, context: MappingContext) {
+    func mapping(var tomap: Person, context: MappingContext) {
+        tomap.attitude <- "traits.attitude" >*<
+        context
+    }
+}
+
+class PersonStub {
+    
+    var bankAccounts: NSArray = [ 0987, 6543 ]
+    var attitude: String = "whoaaaa"
+    
+    init() { }
+    
+    func generateJsonObject() -> Dictionary<String, AnyObject> {
+        return [
+            "traits" : [
+                "attitude" : "chill"
+            ],
+        ]
+    }
+    
+    func matches(object: Person) -> Bool {
+        var matches = true
+        matches &&= attitude == object.attitude
         
+        return matches
     }
 }
