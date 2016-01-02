9cc20de2b08e95cb5855dc285866c3625780cf95
rexmas <rex.fenley@gmail.com>
2016-01-02 15:18:59 -0800

    [remove-realm] removed realm pods

--- Podfile
+++ Podfile
@@ -4,7 +4,6 @@ use_frameworks!
 
 pod 'Alamofire'
 pod 'JSONValueRX'
-pod 'RealmSwift'
 
 link_with 'Crust', 'CrustTests'
 
--- Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -86,12 +86,8 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods/Alamofire.framework"
   install_framework "Pods/JSONValueRX.framework"
-  install_framework "Pods/Realm.framework"
-  install_framework "Pods/RealmSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods/Alamofire.framework"
   install_framework "Pods/JSONValueRX.framework"
-  install_framework "Pods/Realm.framework"
-  install_framework "Pods/RealmSwift.framework"
 fi
