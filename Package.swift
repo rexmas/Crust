1609983a6330716e60ed750155c2abd945ff7152
rex-remind101 <rex@remind101.com>
2018-09-26 16:13:46 -0700

    [swift-4.2] dep bumps

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["Crust"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/rexmas/JSONValue.git", .upToNextMinor(from: "4.1.0"))
+        .package(url: "https://github.com/rexmas/JSONValue.git", .upToNextMinor(from: "4.2.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
